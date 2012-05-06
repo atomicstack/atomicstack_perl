b6a6bcc78e71e8c745be2d893f037b6ecb277891
matt <matt.koscica@gmail.com>
2012-05-06 17:46:39 +0200

    import battery monitor

---
+++ battery.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+use Sys::Hostname qw/hostname/;
+use WebService::Prowl;
+use File::Basename qw/basename/;
+
+my $BATTERY_THRESHOLD = 10;
+
+( my $hostname = hostname() ) =~ s/[.].*//;
+
+my $apikey = $ENV{PROWL_KEY} or die "missing PROWL_KEY environment variable";
+
+my $ws = WebService::Prowl->new(apikey => $apikey);
+
+my $prowl_app      = $hostname;
+my $prowl_event    = basename $0;
+my $prowl_priority = 2;
+my $prowl_description;
+
+my %status;
+
+foreach ( qx{ /usr/sbin/ioreg -nAppleSmartBattery } ) {
+  /\"(MaxCapacity)\"\s+=\s+(\d+)/     and $status{$1} = $2;
+  /\"(CurrentCapacity)\"\s+=\s+(\d+)/ and $status{$1} = $2;
+  /\"(IsCharging)\"\s+=\s+(\w+)/      and $status{$1} = $2;
+}
+
+my $percent_remaining = ( $status{CurrentCapacity} / $status{MaxCapacity} ) * 100;
+$percent_remaining = sprintf '%.02f', $percent_remaining;
+$status{IsCharging} ne 'yes' and $percent_remaining < $BATTERY_THRESHOLD and $prowl_description = "$percent_remaining% remaining";
+
+exit unless $prowl_description;
+
+die $ws->error() unless
+$ws->add('event' => $prowl_event, application => $prowl_app, description => $prowl_description, priority => $prowl_priority);

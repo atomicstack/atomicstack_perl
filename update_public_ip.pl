92c9e70778b759e51aa510e99698709be200b7c1
Matt Koscica <matt.koscica@gmail.com>
2013-06-03 00:23:32 +0200

    lots of updates

--- update_public_ip.pl
+++ update_public_ip.pl
@@ -5,16 +5,17 @@ use 5.14.0;
 use IO::File;
 use Regexp::Common qw/net/;
 use POSIX qw/strftime/;
+use Sys::Hostname;
 
-# use ip.seveas.net (thanks, dennis!) to update an ip history file and track
-# when a host's ip changes
+( my $hostname = hostname() ) =~ s/[.].*//;
+
+# use ip.xort.nl to update an ip history file and
+# track when a host's ip changes
 
 chomp( my @public_ips = IO::File->new($ARGV[0])->getlines );
 my $current_ip;
-my ($current_ip) = map { s/.*?$RE{net}{IPv4}{-keep}.*/$1/r } 
-                 grep { m/whois[-]ip/ }
-                 split /\n/ => qx{wget -q -O - --timeout=5 http://ip.seveas.net};
-                 # IO::File->new("seveas.html" => 'r')->getlines;
+
+chomp ( my $current_ip = qx{/usr/bin/wget -q -O - --timeout=5 http://ip.xort.nl:3000} );
 
 my ($previous_ip) = ( $public_ips[-1] =~ m/$RE{net}{IPv4}{-keep}/ );
 
@@ -23,6 +24,7 @@ chomp($current_ip, $previous_ip);
 exit unless $current_ip =~ m/\A $RE{net}{IPv4} \z/xms;
 
 if ( $previous_ip ne $current_ip ) {
-    warn "$previous_ip ne $current_ip";
+    warn "$previous_ip ne $current_ip" if -t \*STDOUT;
+    system qq|$ENV{HOME}/bin/prowl -a 'update_public_ip.pl' -t $hostname -m "$current_ip"|;
     IO::File->new($ARGV[0] => 'a')->print(strftime("[%F %T] $current_ip\n", localtime));
 }

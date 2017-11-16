1efca0c54cc29d46aa72bbadb9b0215c40ae081a
Matt Koscica <matt.koscica@booking.com>
2017-11-16 11:25:49 +0100

    battery.pl: tweaks

--- battery.pl
+++ battery.pl
@@ -17,12 +17,13 @@ foreach ( qx{ /usr/sbin/ioreg -nAppleSmartBattery } ) {
   /\"(MaxCapacity)\"\s+=\s+(\d+)/           and $status{$1} = $2;
   /\"(CurrentCapacity)\"\s+=\s+(\d+)/       and $status{$1} = $2;
   /\"(ExternalChargeCapable)\"\s+=\s+(\w+)/ and $status{$1} = $2;
+  /\"(ExternalConnected)\"\s+=\s+(\w+)/     and $status{$1} = $2;
 }
 
 my $percent_remaining = ( $status{CurrentCapacity} / $status{MaxCapacity} ) * 100;
 my $percent_remaining = $status{percent_remaining} = sprintf '%.02f', $percent_remaining;
-$status{ExternalChargeCapable} ne 'Yes' and $percent_remaining < $BATTERY_THRESHOLD and $message = "$percent_remaining% remaining";
-# $message = "$percent_remaining% remaining";
+my $should_alert = ( $status{ExternalChargeCapable} ne 'Yes' and $percent_remaining < $BATTERY_THRESHOLD );
+$should_alert and $message = "$percent_remaining% remaining";
 
 $ENV{DEBUG} and print Dumper({
     %status,
@@ -31,7 +32,7 @@ $ENV{DEBUG} and print Dumper({
     battery_is_below_threshold => ( $percent_remaining < $BATTERY_THRESHOLD ),
 });
 
-exit unless $message;
+exit unless ( $should_alert and $message );
 
 my @prowl_command = (
     "$ENV{HOME}/bin/prowl" =>

5807e3a5543c1b7c829bcb1356df197b644c2338
Matt Koscica <matt.koscica@gmail.com>
2021-02-05 19:11:42 +0100

    build_perl.sh: add another env var to toggle no-taint support; minor cleanup/refactor

--- build_perl.sh
+++ build_perl.sh
@@ -52,23 +52,28 @@ get_src
 # assuming this is a freshly built debian box...
 [[ -x "/usr/bin/make" && -x '/usr/bin/cc' ]] || sudo apt-get install build-essential pkg-config autoconf zip unzip bzip2 libssl-dev zlib1g-dev libreadline-dev libexpat-dev libevent-dev libncurses-dev
 
+# can't just use $LATEST_PERL_VERSION to construct $VERSION as we could be
+# installing a user-specified release, not just the latest one
 TARBALL=$(basename $TARBALL_PATH)
 if [[ "$TARBALL_PATH" =~ [.]tar[.]gz$ ]]; then
     VERSION=$(basename $TARBALL_PATH .tar.gz)
-    # TAR_DECOMPRESS_PARAM=z
 elif [[ "$TARBALL_PATH" =~ [.]tar[.]bz2$ ]]; then
     VERSION=$(basename $TARBALL_PATH .tar.bz2)
-    # TAR_DECOMPRESS_PARAM=j
 else
     die "don't know how to strip extension from $TARBALL_PATH"
 fi
 PREFIX=$HOME/$VERSION
+L="$PREFIX/lib"
+
+if [[ -n "$NO_TAINT" || -n "$NO_TAINT_SUPPORT" ]]; then
+  TAINT_FLAGS="-A ccflags=\"-DSILENT_NO_TAINT_SUPPORT\""
+fi
 
 # die "\$URL: $URL, \$TARBALL_PATH: $TARBALL_PATH, \$LOCAL_PATH: $LOCAL_PATH"
 
 pushd "$BUILD_DIR"
 [[ -f "$TARBALL_PATH"    ]] && echo untarring $TARBALL && tar -xf "$TARBALL_PATH" && pushd "$BUILD_DIR/$VERSION"
-[[ -f Configure          ]] && ./Configure -des -Dprefix=$PREFIX -Dinc_version_list=none -Dprivlib=$PREFIX/lib -Darchlib=$PREFIX/lib -Dsitearch=$PREFIX/lib -Dsitelib=$PREFIX/lib && nice make -j 4
+[[ -f Configure          ]] && ./Configure -des -Dprefix=$PREFIX -Dinc_version_list=none -Dprivlib=$L -Darchlib=$L -Dsitearch=$L -Dsitelib=$L $TAINT_FLAGS && nice make -j 4
 [[ -n "$TEST_PERL"       ]] && [[ -f Makefile ]] && [[ -x perl ]] && nice make test && make install
 [[ -z "$TEST_PERL"       ]] && [[ -f Makefile ]] && [[ -x perl ]] && make install
 [[ -x "$PREFIX/bin/perl" ]] && echo -e "PATH=$PREFIX/bin:\$PATH\nnice $PREFIX/bin/cpan App::cpanminus App::cpanoutdated && nice $PREFIX/bin/cpanm https://github.com/atomicstack/Task-BeLike-MATTK/archive/master.zip \$( $PREFIX/bin/cpan-outdated )"

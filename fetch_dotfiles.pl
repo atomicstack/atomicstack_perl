5c7f1e4f06588acd5b85e7fc12b0a54f0701ffbd
Matt Koscica <matt.koscica@gmail.com>
2021-02-05 18:21:29 +0100

    fetch_dotfiles: show verbose SSH output upon git-fetch failure

--- fetch_dotfiles.pl
+++ fetch_dotfiles.pl
@@ -14,7 +14,7 @@ my ($hostname) = map { s/[.].*//r } hostname();
 my ($key_filename) = grep { m/${hostname}_dotfiles_deploy_\d{4}-\d{2}-\d{2}/ } glob "$ENV{HOME}/.ssh/*dotfiles_deploy*.pem";
 die "couldn't find key filename :(" unless ( $key_filename and -f $key_filename );
 
-$ENV{GIT_SSH_COMMAND} = qq{ssh -o ClearAllForwardings=yes -o "IdentitiesOnly=yes" -i $key_filename};
+$ENV{GIT_SSH_COMMAND} = qq{ssh -v -o ClearAllForwardings=yes -o "IdentitiesOnly=yes" -i $key_filename};
 my @output = qx{git fetch --all 2>&1};
 my $exit_signal = $? >> 127;
 my $exit_code = $? >> 8;
@@ -22,4 +22,4 @@ my $exit_code = $? >> 8;
 if    ($exit_signal) { warn "git fetch died of signal $exit_signal" }
 elsif ($exit_code)   { warn "git fetch terminated with exit code $exit_code" }
 
-# say @output;
+( $exit_signal or $exit_code ) and say @output;
